3e49677d5206d09ab1bad918e0608ec99503fb69
Tim Baxter <tim.baxter@cerner.com>
2015-01-15 17:17:24 -0600

    function. it's a function

--- fretboard/settings.py
+++ fretboard/settings.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
 
-def current_site():
+def get_current_site():
     """
     Helper function to play nice with appconfig loading.
     """
@@ -9,5 +9,5 @@ def current_site():
 
 
 PAGINATE_BY = getattr(settings, "PAGINATE_BY", 25)
-FORUM_BASE_NAME  = getattr(settings, 'FORUM_BASE_NAME', current_site.name)
+FORUM_BASE_NAME  = getattr(settings, 'FORUM_BASE_NAME', get_current_site().name)
 COMMENT_PLACEHOLDER = getattr(settings, "COMMENT_PLACEHOLDER", "Be nice.")
